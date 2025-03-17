1f8a8f76756148fcf62015f63641bc96119e45dd
parshant-sharma <parshant.unifiers@gmail.com>
2025-03-17 12:54:00 +0530

    issue solve

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRoute from "./src/routing/AppRoute";
-import "./public/style.css";
+import "./src/style.css";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
