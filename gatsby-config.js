6f39e5bd4999bf631e9f012b643874ccf2bcf362
parshant-sharma <parshant.unifiers@gmail.com>
2025-03-17 17:16:06 +0530

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,6 @@
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://cae-21.netlify.app`,
   },
   plugins: [],
-}
+};
