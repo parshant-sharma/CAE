f5dafe2dc315d17251ee7473025435a0ee56ff69
parshant-sharma <parshant.unifiers@gmail.com>
2025-03-17 12:19:52 +0530

    Initial commit

--- src/routing/AppRoute.js
+++ src/routing/AppRoute.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Routes , Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../commponents/Home";
-const AppRoute =()=>{
-
-    return(<>
-     <Routes>
-        <Route path="/" element={<Home/>}/>
-        {/* <Route path="*" element={{hllo}}/> */}
-    </Routes>
-    </>);
-   
-
-}
+import NotFoundPage from "../pages/404";
+const AppRoute = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+};
 
 export default AppRoute;
