f6c8fed6f5efaceba3e0353702a93817a5a498da
parshant-sharma <parshant.unifiers@gmail.com>
2025-03-18 11:14:53 +0530

    first commit

---
+++ gatsby-browser.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoute from "./src/routing/AppRoute";
+import "./src/style.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <BrowserRouter>
+      <AppRoute />
+    </BrowserRouter>
+  );
+};
---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://cae-21.netlify.app/`,
+  },
+  plugins: [],
+};
