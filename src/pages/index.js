827d349abaf8a54b206e6fa318643ce9b43b289e
parshant-sharma <parshant.unifiers@gmail.com>
2025-03-18 15:24:06 +0530

    push

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AppRoute from "../routing/AppRoute";
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to My Gatsby Site with React Router!</h1>
+    <AppRoute />
+  </div>
+);
+
+export default IndexPage;
