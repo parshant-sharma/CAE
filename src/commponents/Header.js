973c5b31aa9862e4ce3e1c36d11b1bfcaf2230b7
parshant-sharma <parshant.unifiers@gmail.com>
2025-03-18 14:48:39 +0530

    first commit

--- src/commponents/Header.js
+++ src/commponents/Header.js
@@ -1,49 +1,112 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import CAE_LOGO from "../images/CAE_logo.png";
+
 const Header = () => {
   return (
-    <div style={{ justifyItems: "center" }}>
+    <div style={{ display: "flex", justifyItems: "center", width: "100%" }}>
       <header
-        style={{ width: "80%" }}
-        className="bg-white py-10 relative w-[80%] mx-auto"
+        style={{
+          width: "80%",
+          backgroundColor: "white",
+          paddingTop: "2.5rem",
+          paddingBottom: "2.5rem",
+          position: "relative",
+          margin: "0 auto",
+        }}
       >
         <img
           src={CAE_LOGO}
           alt="CAE Logo"
-          className="absolute  top-1/2 transform -translate-y-1/2 w-24"
+          style={{
+            position: "absolute",
+            top: "50%",
+            transform: "translateY(-50%)",
+            width: "6rem",
+          }}
         />
 
         <nav>
-          <ul className="flex justify-center space-x-6 md:space-x-10">
-            <li>
+          <ul
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              listStyleType: "none",
+              paddingLeft: 0,
+            }}
+          >
+            <li style={{ marginRight: "1.5rem" }}>
               <Link
                 to="#home"
-                className="text-blue-600 text-lg font-medium hover:text-orange-400"
+                style={{
+                  color: "#1e40af", // blue-600
+                  fontSize: "1.125rem", // text-lg
+                  fontWeight: "500", // font-medium
+                  textDecoration: "none",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.color = "#f97316"; // hover:text-orange-400
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.color = "#1e40af"; // revert to blue-600
+                }}
               >
                 Home
               </Link>
             </li>
-            <li>
+            <li style={{ marginRight: "1.5rem" }}>
               <Link
                 to="#services"
-                className="text-black text-lg font-medium hover:text-orange-400"
+                style={{
+                  color: "#000", // black
+                  fontSize: "1.125rem", // text-lg
+                  fontWeight: "500", // font-medium
+                  textDecoration: "none",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.color = "#f97316"; // hover:text-orange-400
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.color = "#000"; // revert to black
+                }}
               >
                 Services
               </Link>
             </li>
-            <li>
+            <li style={{ marginRight: "1.5rem" }}>
               <Link
                 to="#contact"
-                className="text-black text-lg font-medium hover:text-orange-400"
+                style={{
+                  color: "#000", // black
+                  fontSize: "1.125rem", // text-lg
+                  fontWeight: "500", // font-medium
+                  textDecoration: "none",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.color = "#f97316"; // hover:text-orange-400
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.color = "#000"; // revert to black
+                }}
               >
                 Contact
               </Link>
             </li>
-            <li>
+            <li style={{ marginRight: "1.5rem" }}>
               <Link
                 to="#about"
-                className="text-black text-lg font-medium hover:text-orange-400"
+                style={{
+                  color: "#000", // black
+                  fontSize: "1.125rem", // text-lg
+                  fontWeight: "500", // font-medium
+                  textDecoration: "none",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.color = "#f97316"; // hover:text-orange-400
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.color = "#000"; // revert to black
+                }}
               >
                 About
               </Link>
@@ -51,7 +114,18 @@ const Header = () => {
             <li>
               <Link
                 to="#support"
-                className="text-black text-lg font-medium hover:text-orange-400"
+                style={{
+                  color: "#000", // black
+                  fontSize: "1.125rem", // text-lg
+                  fontWeight: "500", // font-medium
+                  textDecoration: "none",
+                }}
+                onMouseEnter={(e) => {
+                  e.target.style.color = "#f97316"; // hover:text-orange-400
+                }}
+                onMouseLeave={(e) => {
+                  e.target.style.color = "#000"; // revert to black
+                }}
               >
                 Support
               </Link>
