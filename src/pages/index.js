c67aece4243afc0d9c5494c3ac91f7d98b53806a
parshant-sharma <parshant.unifiers@gmail.com>
2025-03-18 15:30:16 +0530

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom"; // Import BrowserRouter
 import AppRoute from "../routing/AppRoute";
 
 const IndexPage = () => (
-  <div>
-    <h1>Welcome to My Gatsby Site with React Router!</h1>
-    <AppRoute />
-  </div>
+  <Router>
+    {" "}
+    {/* Wrap your AppRoute in BrowserRouter */}
+    <div>
+      <h1>Welcome to My Gatsby Site with React Router!</h1>
+      <AppRoute />
+    </div>
+  </Router>
 );
 
 export default IndexPage;
--- src/routing/AppRoute.js
+++ src/routing/AppRoute.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../commponents/Home";
 import NotFoundPage from "../commponents/404";
+
 const AppRoute = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 };
 
